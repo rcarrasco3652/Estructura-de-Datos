440b186dc44a41b2f842f147aa6ce2dddef9a591
rcarrasco03652 <rcarrasco0@ucol.mx>
2019-09-02 02:29:04 -0500

    termine creo xD

--- ActividadClase5/js/Punto.js
+++ ActividadClase5/js/Punto.js
@@ -1,11 +1,34 @@
-export default class Punto{
-    constructor(){
-        this._x = 0
-        this._y = 0
+export default class Punto {
+    constructor(id) {
+        this._id = id;
+        this._x = 0;
+        this._y = 0;
+    }
+    get x() {
+        return this._x;
+    }
+    set nuevaX(valX,) {
+        if (valX <= 100 && valX >= -100) {
+            this._x = valX;
+        } else {
+            return;
+        }
     }
 
-    get x(){
-        return this._x
+    get y() {
+        return this._y;
     }
-    set x()
+    set nuevaY(valY) {
+        if (valY <= 100 && valY >= -100) {
+            this._y = valY;
+        }else{
+            return
+        }
+    }
+
+    geToStrings(){
+        let imprimir = `La posicion del punto ${this._id} es (${this._x},${this._y})`
+        return imprimir
+    }
+
 }
--- ActividadClase5/js/main.js
+++ ActividadClase5/js/main.js
@@ -1,9 +1,36 @@
+import Punto from "./Punto.js";
+let posUno = new Punto(1);
+let posDos = new Punto(2);
 
+class Main {
+    constructor() {
 
-class Main{
-    constructor(){
+        document.getElementById('btn').addEventListener('click', () => {
+            let xUno = Number(document.getElementById('xUno').value);
+            let yUno = Number(document.getElementById('yUno').value);
+            let xDos = Number(document.getElementById('xDos').value);
+            let yDos = Number(document.getElementById('yDos').value);
+            let print = document.querySelector("#print")
 
+            print.innerHTML += nuevasCordenadas(xUno, yUno, xDos, yDos);
+        })
+
+        function nuevasCordenadas(xUno, yUno, xDos, yDos) {
+            posUno.nuevaX = xUno;
+            posUno.nuevaY = yUno;
+
+            posDos.nuevaX = xDos;
+            posDos.nuevaY = yDos
+
+            return `${posUno.geToStrings()} , ${posDos.geToStrings()} y la distancia entre los dos puntos es de: ${distancia()}`
+        }
+
+        function distancia() {
+            let distancia = Math.sqrt(
+                Math.pow((posDos.x - posUno.x), 2) + Math.pow((posDos.y - posUno.y), 2)
+                );
+            return distancia;
+        }
     }
-
-    
 }
+let m = new Main;
