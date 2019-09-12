6d94a5b8216422a5409ed199a60af4fbe632c1d8
20153652 CARRASCO MEDINA ROBERTO FAUST <20153652@TELEMATICA.UCOL.MX>
2019-09-12 15:46:13 -0500

    vfbvd

--- Tarea5/js/main.js
+++ Tarea5/js/main.js
@@ -7,19 +7,15 @@ class Main{
         let pPuerta=document.querySelector("#pPuerta");
         let pMensaje=document.querySelector("#pMensaje");
 
-        pTotalPisos.textContent = this._elevador.totalPisos;
-        pPisoActual.textContent = this._elevador.pisoActual;
-        pPuerta.textContent = this._elevador.toString(this._elevador.puerta);
-        pMensaje.textContent = "Beinvenido al elevador";
 
         document.querySelector("#btnAbrir").addEventListener("click", () =>{
             if(this._elevador.puerta===false){
                 this._elevador.abrirPuerta();
-                pMensaje.textContent = "Se ha abierto la puerta";
-                pPuerta.textContent = this._elevador.toString(this._elevador.puerta);
+                console.log("Se ha abierto la puerta")
+                console.log(this._elevador.toString(this._elevador.puerta))
                 return;
             }else{
-                pMensaje.textContent = "La puerta ya estaba abierta";
+                console.log("La puerta ya estaba abierta");
                 return;
             }
         });
@@ -27,11 +23,11 @@ class Main{
         document.querySelector("#btnCerrar").addEventListener("click", () =>{
             if(this._elevador.puerta===true){
                 this._elevador.cerrarPuerta();
-                pMensaje.textContent = "Se ha cerrado la puerta";
-                pPuerta.textContent = this._elevador.toString(this._elevador.puerta);
+                console.log("Se ha cerrado la puerta");
+                console.log(this._elevador.toString(this._elevador.puerta));
                 return;
             }else{
-                pMensaje.textContent = "La puerta ya estaba cerrada";
+                console.log("La puerta ya estaba cerrada");
                 return;
             }
         });
@@ -40,20 +36,20 @@ class Main{
             let iPiso = document.querySelector("#iPiso").value;
 
             if(iPiso===this._pisoActual){
-                pMensaje.textContent = "El elevador ya estaba en el piso " + iPiso;
+                console.log("El elevador ya estaba en el piso " + iPiso);
                 return;
             }else if(iPiso>this._elevador.totalPisos){
-                pMensaje.textContent = "Piso inexistente. Numero de pisos totales: del 0 al "+ this._elevador.totalPisos
+                console.log("Piso inexistente. Numero de pisos totales: del 0 al "+ this._elevador.totalPisos)
             }else if(this._elevador.puerta===true){
-                pMensaje.textContent = "No se puede mover con la puerta abierta, favor de cerrarla";
+                console.log("No se puede mover con la puerta abierta, favor de cerrarla");
                 return
             }else if(iPiso==""){
-                pMensaje.textContent = "Inserte un numero de piso antes de mover el elvador";
+                console.log("Inserte un numero de piso antes de mover el elvador");
                 return
             }else{
                 this._elevador.pisoActual=iPiso;
-                pMensaje.textContent = "El elevador se ha movido al piso " + this._elevador.pisoActual;
-                pPisoActual.textContent = this._elevador.pisoActual;
+                console.log("El elevador se ha movido al piso " + this._elevador.pisoActual);
+                console.log(this._elevador.pisoActual);
                 return
             }
         });
