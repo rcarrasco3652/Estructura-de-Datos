a183efbfd13bb0bc92e543a5553da64660f2b26a
20153652 CARRASCO MEDINA ROBERTO FAUST <20153652@TELEMATICA.UCOL.MX>
2019-10-07 16:02:26 -0500

    Trabajo en clase

---
+++ ActividadClase8/main.js
@@ -0,0 +1,6 @@
+function fact(x){
+    if(x<=1)
+        return 1;
+    else
+        return x * fact(x-1)
+}
--- Tarea9/js/Lista.js
+++ Tarea9/js/Lista.js
@@ -95,16 +95,15 @@ export default class Lista {
             articuloTemporal = articuloTemporal.siguiente;
         }
     }
-    _imprimirArticuloEncontrado(articulo,lienzo){
-        lienzo.innerHTML = ""
-        lienzo.innerHTML += `
+    _imprimirArticuloEncontrado(articulo){
+        return `
         Codigo: ${articulo.codigo} <br>
         Nombre: ${articulo.nombre} <br>
         Precio: ${articulo.precio} <br>
         Contenido: ${articulo.contenido}gr <br>
         Descripcion: ${articulo.descripcion}`
-
     }
+    
 }
 
 
